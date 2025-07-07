61b14d239909787c5061ec69bda1d8972dd104ca
cindymikoda <mikodacindy@gmail.com>
2025-07-07 16:57:42 +0100

    Add animated background to index.html, update form handling in JavaScript, and enhance CSS styles for improved aesthetics and layout

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
+console.log("Script loaded!");
 
+function generatePoem(event) {
+  event.preventDefault();
+
+  let thePoemContent = document.querySelector("#thePoem");
+
+  console.log("Form submitted");
+
+  thePoemContent.innerHTML = "";
+
+  new Typewriter(thePoemContent, {
+    strings: ["Title", "a very short poem"],
+    autoStart: true,
+    delay: 15,
+  });
+}
+
+let poemFormElement = document.querySelector("#poemForm");
+console.log(poemFormElement); // make sure it's not null
+poemFormElement.addEventListener("submit", generatePoem);
